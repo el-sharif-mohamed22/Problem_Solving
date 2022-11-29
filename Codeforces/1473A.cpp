a65b03838a622590d059ea48c4438272229d4e30
el-sharif-mohamed22 <95565086+el-sharif-mohamed22@users.noreply.github.com>
2022-11-29 16:00:06 +0200

    new file:   Codeforces/1473A.cpp

---
+++ Codeforces/1473A.cpp
@@ -0,0 +1,29 @@
+// problem link: https://codeforces.com/problemset/problem/1473/A
+// problem name: A. Replacing Elements, 1473A
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    short tt; cin >> tt;
+    while (tt--) {
+        bool satisfy{true};
+        short n, d, min1{101}, min2{101}; cin >> n >> d;
+        vector<short> arr(n);
+        for (short& i : arr) {
+            cin >> i;
+            if (i > d) satisfy = false;
+            if (i < min1) {
+                min2 = min1;
+                min1 = i;
+            } else if (i < min2) {
+                min2 = i;
+            }
+        }
+        cout << (min1 + min2 <= d || satisfy ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
