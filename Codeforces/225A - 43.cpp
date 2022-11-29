16501734a9882a13e553993f182520499ba9a9bb
el-sharif-mohamed22 <95565086+el-sharif-mohamed22@users.noreply.github.com>
2022-11-29 15:42:39 +0200

    new file:   Codeforces/225A - 43.cpp

---
+++ Codeforces/225A - 43.cpp
@@ -0,0 +1,23 @@
+// problem link: https://codeforces.com/problemset/problem/225/A
+// problem name: A. Dice Tower, 225A - 43
+#include <iostream>
+#include <string>
+using namespace std;
+string solve() {
+    int n, x, a, b; cin >> n >> x;
+    for (int i = 0; i < n; ++i) {
+        cin >> a >> b;
+        if (a == x || (7 - a) == x || b == x || (7 - b) == x) {
+            return "NO";
+        }
+    }
+    return "YES";
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << solve();
+    return 0;
+}
