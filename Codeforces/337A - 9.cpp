0560b15e2a12aae55029b8eda86d1a6025eaef07
el-sharif-mohamed22 <95565086+el-sharif-mohamed22@users.noreply.github.com>
2022-12-13 07:44:16 +0200

    Changes to be committed: 	new file:   Codeforces/337A - 9.cpp 	new file:   Codeforces/34B - 4.cpp

---
+++ Codeforces/337A - 9.cpp
@@ -0,0 +1,23 @@
+// Problem name: 337A - 9, Puzzles
+// problem link: https://codeforces.com/problemset/problem/337/A
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n, m, diff{ INT_MAX }; cin >> n >> m;
+    vector<int> arr(m);
+    for (int& i : arr) {
+        cin >> i;
+    }
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i <= m - n; ++i) {
+        diff = min(diff, arr[i + n - 1] - arr[i]);
+    }
+    cout << diff;
+}
---
+++ Codeforces/34B - 4.cpp
@@ -0,0 +1,28 @@
+// Problem name: 34B - 4, Sale
+// problem link: https://codeforces.com/contest/34/problem/B
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    short n, m; cin >> n >> m;
+    vector<int> arr(n);
+    for (int& i : arr) {
+        cin >> i;
+    }
+    sort(arr.begin(), arr.end());
+    long long sum{0};
+    for (int i = 0; i < m; ++i) {
+        if (arr[i] < 0) {
+            sum -= arr[i];
+        } else {
+            break;
+        }
+    }
+    cout << sum << "\n";
+}
